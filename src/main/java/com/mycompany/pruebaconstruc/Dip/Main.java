0ee68384531d2f618eeead5e3ef3b4a7acb39eec
Tito Nova <tnova@mysanitas.com>
2023-02-23 18:41:40 -0500

    [INIT] changues

--- src/main/java/com/mycompany/pruebaconstruc/Dip/Main.java
+++ src/main/java/com/mycompany/pruebaconstruc/Dip/Main.java
@@ -10,7 +10,7 @@ import java.util.Date;
  *
  * @author USUARIO
  */
-public class Main {
+public class main {
    
     public static void main(String[] args) {
         // Creamos un calculador de deducción y un calculador de IVA
